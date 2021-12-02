1c16da5127e3c62a2d45642b4e3eb814048f2e2d
rajivkumar <rajmittal09@gmail.com>
2021-12-02 13:20:55 +0000

    modified example as per yanka example

--- example.py
+++ example.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-# from streamlit_webrtc import webrtc_streamer
+from streamlit_webrtc import webrtc_streamer
 
-# webrtc_streamer(key="example")
+webrtc_streamer(key="example")
