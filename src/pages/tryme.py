0877b1cdbfe3977e50353c65952fca28ae48e4cb
rajivkumar <rajmittal09@gmail.com>
2021-12-02 14:59:18 +0000

    test model path

--- src/pages/tryme.py
+++ src/pages/tryme.py
@@ -32,24 +32,25 @@ def retrieve_model(PATH_MODEL, PATH_LABEL):
     # label = np.load(PATH_LABEL, allow_pickle=True)
     return model, label
 
-print(os.getcwd())
-PATH_MODEL_ASL = "./saved_models/asl_model2.h5"
-PATH_LABEL_ASL = "./saved_labels/asl_class_names2.txt"
+# print(os.getcwd())
+# PATH_MODEL_ASL = "./saved_models/asl_model2.h5"
+# PATH_LABEL_ASL = "./saved_labels/asl_class_names2.txt"
 
-model_asl, label_asl = retrieve_model(PATH_MODEL_ASL, PATH_LABEL_ASL)
+# model_asl, label_asl = retrieve_model(PATH_MODEL_ASL, PATH_LABEL_ASL)
 
-PATH_MODEL_ARABIC = "./saved_models/arabic_model.h5"
-PATH_LABEL_ARABIC = "./saved_labels/class_name_arabic.txt"
+# PATH_MODEL_ARABIC = "./saved_models/arabic_model.h5"
+# PATH_LABEL_ARABIC = "./saved_labels/class_name_arabic.txt"
 
-model_arabic, label_arabic = retrieve_model(PATH_MODEL_ARABIC,
-                                            PATH_LABEL_ARABIC)
+# model_arabic, label_arabic = retrieve_model(PATH_MODEL_ARABIC,
+#                                             PATH_LABEL_ARABIC)
 
 
-def write(mas,
-          lasl=label_asl,
-          marab=model_arabic,
-          larab=label_arabic):
+def write(mas):
+        #   lasl=label_asl,
+        #   marab=model_arabic,
+        #   larab=label_arabic):
     st.write(mas.summary())
+    print(mas.summary())
     options = st.selectbox("Choose what you want to test today",
                            index=0,
                            options=[
